8a5f6c01b43835abdb7b973b8cd03a35476c0423
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-04-24 16:51:35 -0400

    notes show correctly, non-functioning add new note button

--- src/components/myApplyances/MyApplyanceDetails.js
+++ src/components/myApplyances/MyApplyanceDetails.js
@@ -7,15 +7,27 @@ export const MyApplyanceDetails = () => {
 
     const { myApplyanceId } = useParams()
 
-    const [applyance, updatedApplyance] = useState({
+    const [applyance, updatedApplyance] = useState({})
 
-    })
+    const [notes, setNotes] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/notes?applyanceId=${myApplyanceId}&_expand=applyance&_sort=id&_order=desc`)
+                .then(response => response.json())
+                .then((notesArray) => {
+                    setNotes(notesArray)
+                })
+        },
+        []
+    )
+
 
     // get apply user object out of local storage
     const localApplyUser = localStorage.getItem("apply_user") // a string
     const applyUserObject = JSON.parse(localApplyUser) // an object with 2 keys (id and staff)
 
-    
+
     const getAllApplyances = () => {
         fetch(`http://localhost:8088/applyances?_sort=makeModel&_expand=user`)
             .then(response => response.json())
@@ -41,31 +53,31 @@ export const MyApplyanceDetails = () => {
     const editButton = () => {
         if (applyUserObject.id === applyance.userId || applyUserObject.admin === true) {
             return <>
-            <footer><Link to={`/all-applyances-edit/${myApplyanceId}`}><button>
-    Edit ApplYance
-    </button></Link></footer> 
+                <footer><Link to={`/all-applyances-edit/${myApplyanceId}`}><button>
+                    Edit ApplYance
+                </button></Link></footer>
             </>
         } else {
             return ""
         }
     }
-    
+
     const deleteButton = () => {
         if (applyUserObject.id === applyance.userId || applyUserObject.admin === true) {
             return <>
-            <footer><button
-                        onClick={() => {
-                            fetch(`http://localhost:8088/applyances/${myApplyanceId}`, {
-                                method: "DELETE"
-                                }) 
+                <footer><button
+                    onClick={() => {
+                        fetch(`http://localhost:8088/applyances/${myApplyanceId}`, {
+                            method: "DELETE"
+                        })
                             .then(() => {
-                            getAllApplyances()
+                                getAllApplyances()
                             })
-                        }}
-            
-            >
-    Delete ApplYance
-    </button></footer> 
+                    }}
+
+                >
+                    Delete ApplYance
+                </button></footer>
             </>
         } else {
             return ""
@@ -73,32 +85,51 @@ export const MyApplyanceDetails = () => {
     }
 
     return (<article className="detailsArticle">
-    <section className="applyanceSection">
-        <div>
-            <header className="applyance__header">{applyance.makeModel}</header>
-            <div className="tag">{applyance?.tag?.location}</div>
-            <div className="manualLink">
-                <Link to={applyance.manual}>Link to Manual</Link>
+        <section className="applyanceSection">
+            <div>
+                <header className="applyance__header">{applyance.makeModel}</header>
+                <div className="tag">{applyance?.tag?.location}</div>
+                <div className="manualLink">
+                    <Link to={applyance.manual}>Link to Manual</Link>
+                </div>
             </div>
-        </div>
-        
-        <div>
-            <img src={`${applyance.picture}`}></img>
-        </div>
-        
-        <div>
-            <div>Owner: {applyance?.user?.firstName}</div>
-            <div className="purchaseDiv">Purchase Information:
-                <div>Purchase Date: {applyance.purchaseDate}</div>
-                <div>Purchase Price: ${applyance.purchasePrice}</div>
-                <div>Purchase Location: {applyance.purchaseLocation}</div>
+
+            <div>
+                <img src={`${applyance.picture}`}></img>
             </div>
+
+            <div>
+                <div>Owner: {applyance?.user?.firstName}</div>
+                <div className="purchaseDiv">Purchase Information:
+                    <div>Purchase Date: {applyance.purchaseDate}</div>
+                    <div>Purchase Price: ${applyance.purchasePrice}</div>
+                    <div>Purchase Location: {applyance.purchaseLocation}</div>
+                </div>
             </div>
-            
-        <footer>
+
+            <footer>
                 {editButton()}
                 {deleteButton()}
             </footer>
-    </section>
+        </section>
+        <section className="applyanceNotes">
+            <button>Add New Note</button>
+            <p></p>
+            Notes:
+            <div>
+                {
+                    myApplyanceId ?
+                        notes.map(
+                            (note) => {
+                                return <div key={note.id} className="note">
+                                    {note.description}
+                                </div>
+                            }
+                        )
+                        : ""
+                }
+
+            </div>
+        </section>
     </article>)
 }
