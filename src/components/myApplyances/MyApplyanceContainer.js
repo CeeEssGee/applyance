cc4afa3718f6e4d849f25b64fd80f5bddeac1fb3
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-05-02 17:04:07 -0400

    abandoned this stretch goal

--- src/components/myApplyances/MyApplyanceContainer.js
+++ src/components/myApplyances/MyApplyanceContainer.js
@@ -9,15 +9,15 @@ import { ApplyanceSearch } from "../applyances/ApplyanceSearch"
 export const MyApplyanceContainer = () => {
 
     // useState hook maintains a state variable (mySearchTerms) which is initially an empty string, passes mySetSearchTerms function into the MyApplyanceSearch child component as a prop (setterFunction)
-    const [mySearchTerms, mySetSearchTerms] = useState("")
+    const [searchTerms, setSearchTerms] = useState("")
 
     return <>
 
     
         {/* MyApplyanceSearch component allows a user to input search terms by typing into a text field, which calls the mySetSearchTerms function that was passed down and updates the mySearchTerms state variable */}
-        <ApplyanceSearch mySetterFunction={mySetSearchTerms}/>
+        <ApplyanceSearch setterFunction={setSearchTerms}/>
 
         {/* MyApplyanceList component receives the mySearchTermState prop from the parent and uses it to filter the list of applyances that it displays. If empty, it displays all my applyances. Otherwise, it displays a filtered list based on the whether the make/model includes the mySearchTerms string */}
-        <MyApplyanceList mySearchTermState={mySearchTerms}/>
+        <MyApplyanceList searchTermState={searchTerms}/>
     </>
 }
